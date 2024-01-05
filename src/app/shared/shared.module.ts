e401111adbe28e9c4fb780de4498a67675be2c43
Camilo Gongora Ortega <camilo.gongora@softtekprojects.com>
2024-01-05 16:18:04 -0300

    first commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,26 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { MediaPlayerComponent } from './components/media-player/media-player.component';
+import { HeaderUserComponent } from './components/header-user/header-user.component';
+
+
+
+@NgModule({
+  declarations: [
+    SideBarComponent,
+    MediaPlayerComponent,
+    HeaderUserComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports:[
+    SideBarComponent,
+    MediaPlayerComponent,
+    HeaderUserComponent,
+  ]
+})
+export class SharedModule { }
